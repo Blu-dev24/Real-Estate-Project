63c716056973318eb24e602fb138798e9e7f35d7
payaalarunsingh@gmail.com <payaalarunsingh>
2025-11-02 15:42:15 +0530

    Real Estate Project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './components/Header.jsx'
+import Hero from './components/Hero.jsx'
+
+const App = () => {
+  return (
+    <>
+    <Header/>
+    <main>
+      <Hero/>
+    </main>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react'
+import { RiCloseFill, RiMenuFill } from '@remixicon/react';
+import { navItems } from '../constant/data'
+import Button from './Button';
+
+
+const Header = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const handleClick = () => {
+      setIsOpen(!isOpen);
+    }
+  return (
+    <header className="header">
+      <div className="container flex items-center justify-between">
+        <a href="#">
+          <img src="/images/Logo.png" alt="logo" width={115} height={59} />
+        </a>
+
+        {/* Mobile Menu */}
+        <nav className={`navbar ${isOpen ? 'active' : ''}`}>
+          <button className="ml-auto mb-10" onClick={handleClick}>
+            <RiCloseFill size={30} />
+          </button>
+
+          <ul className="flex flex-col flex-1 justify-center items-center gap-10">
+            {navItems.map(item => (
+              <li key={item.id}>
+                <a href={item.href} className='hover:text-sky-600 transition-colors text-lg font-medium'>{item.label}</a>
+              </li>
+            ))}
+          </ul>
+
+          <Button label='Get Started' className='secondary-btn' />
+        </nav>
+
+        {/* Menu Toggle */}
+        <button className="md:hidden" onClick={handleClick}>
+          <RiMenuFill size={30} />
+        </button>
+
+        {/* lg Menu */}
+        <ul className="hidden md:flex gap-10 md:items-center">
+          {navItems.map(item => (
+            <li key={item.id}>
+              <a href={item.href} className='hover:text-gray-200 font-medium transition-colors'>{item.label}</a>
+            </li>
+          ))}
+
+          <Button label='Get Started' className='primary-btn' />
+        </ul>
+
+        {/* Overlay */}
+          <div className={`overlay ${isOpen ? 'active' : ''}`} onClick={handleClick}></div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
