cd0fa8ed981ce4c186cd5fe67c10fb310b71ca20
payaalarunsingh@gmail.com <payaalarunsingh>
2025-12-04 17:51:10 +0530

    major ui changes

--- src/components/PopularProperties.jsx
+++ src/components/PopularProperties.jsx
@@ -11,15 +11,16 @@ import 'swiper/css/pagination'
 
 const PopularProperties = () => {
     return (
-        <section className="section pb-[90px] md:pb-[150px]" id='properties'>
+        <section className="section pb-[90px] md:pb-[150px] bg-gradient-to-br from-[#002057] to-[#F5F3EF]
+" id='properties'>
             <div className="container">
                 {/* Title wrapper */}
                 <div className="flex justify-center items-center lg:justify-between lg:items-center">
 
                     <div className="text-center max-w-[600px] mx-auto">
-                        <h2>Popular Properties</h2>
-                        <p className="max-w-[500px] mt-4 mb-7 mx-auto">
-                            Lorem ipsum dolor sit amet consectetur. Faucibus tristique auctor mauris velit varius. Eu duis viverra.
+                        <h2 className='text-[34px] sm:text-[42px] lg:text-[50px] font-medium tracking-wider text-[#00163D]'>Premium Properties</h2>
+                        <p className="max-w-[500px] mt-4 mb-7 mx-auto text-white">
+                            Discover handpicked properties that combine modern living, prime locations, and unmatched value.
                         </p>
                     </div>
 
@@ -47,19 +48,22 @@ const PopularProperties = () => {
                             <div className="">
                                 {/* Card Image */}
                                 <div className="">
-                                    <img src={item.imgURL} alt={item.name} className='w-full h-full object-cover' />
+                                    <img src={item.imgURL} alt={item.name} className='w-full h-full object-cover rounded-xl' />
                                 </div>
 
                                 {/* Card Content */}
                                 <div className="">
-                                    <b className=''>{item.price}</b>
-                                    <a href="#" className=''>{item.name}</a>
-                                    <p>{item.location}</p>
+                                    <div className='flex justify-between'>
+                                        <a href="#" className='text-[18px] text-black'> {item.name}</a>
+                                        <b className='text-[20px]'>{item.price}</b>
+                                    </div>
 
-                                    <div className="">
-                                        <p>{item.bed}</p>
-                                        <p>{item.bath}</p>
-                                        <p>{item.area}</p>
+                                    <p className='text-black' >{item.location}</p>
+
+                                    <div className="grid grid-cols-3 pb-20 mt-5">
+                                        <p className='text-black'>{item.bed}</p>
+                                        <p className='text-black'>{item.bath}</p>
+                                        <p className='text-black'>{item.area}</p>
                                     </div>
                                 </div>
                             </div>
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -3,30 +3,30 @@ import { ServicesCardItem } from '../constant/data'
 
 const Services = () => {
   return (
-    <section className="py-[90px] bg-gray-200/30 md:py-[50px]">
-        <div className="container">
-            <p className="subtitle">
-                Services
-            </p>
-            <h2>Services we offer</h2>
+    <section className="py-[90px] bg-[#e9eff2] md:py-[50px]">
+      <div className="container">
+        <p className="subtitle text-2xl">
+          Services
+        </p>
+        <h2>Services we offer</h2>
 
-            {/* lists */}
-            <ul className="grid gap-6 mt-10 md:mt-16 sm:grid-cols-2 xs:grid-cols-2 lg:grid-cols-4">
-                {ServicesCardItem.map(item=>(
+        {/* lists */}
+        <ul className="grid gap-6 mt-10 md:mt-16 sm:grid-cols-2 xs:grid-cols-2 lg:grid-cols-4">
+          {ServicesCardItem.map(item => (
 
-                    // card
-                    <div className="border border-gray-300 p-9 rounded-lg bg-white hover:border-sky-400 transition" key={item.id}>
-                    <div className='bg-sky-200/30 max-w-max p-5 rounded-full font-bold my-3'>
-                        <img src={item.icon} alt={item.title} width={70} height={70} />
-                    </div>
-                    <h4 className='text-[22px]'>{item.title}</h4>
-                    <p>{item.text}</p>
-                  </div>
-                  
+            // card
+            <div className="border border-gray-300 p-9 rounded-lg bg-white hover:border-sky-400 transition" key={item.id}>
+              <div className='bg-sky-200/30 max-w-max p-5 rounded-full font-bold my-3'>
+                <img src={item.icon} alt={item.title} width={70} height={70} />
+              </div>
+              <h4 className='text-[22px]'>{item.title}</h4>
+              <p>{item.text}</p>
+            </div>
 
-                ))}
-            </ul>
-        </div>
+
+          ))}
+        </ul>
+      </div>
     </section>
   )
 }
