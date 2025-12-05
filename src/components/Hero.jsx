9defdf6e5cef33cab7bd4e043a9a1e3059631304
payaalarunsingh@gmail.com <payaalarunsingh>
2025-12-05 18:30:56 +0530

    hero section ui changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,10 +10,10 @@ const Hero = () => {
             <div className="container mx-auto px-4 text-gray-50">
                 <h1 className='text-4xl sm:text-5xl md:text-6xl lg:text-[64px]'>Find the <br /> Property That <br /> Feels Like Home.</h1>
                 {/* <p className="text-gray-200 max-w-[440px] pt-4 pb-8">From cozy apartments to spacious family homes, we'll help you find the one that fits your lifestyle.</p> */}
-                <Button label='Search Property' className="px-8 py-4 mt-8 rounded-full text-white font-semibold bg-white/10 backdrop-blur-md border border-white/30 hover:bg-white/20 transition-all duration-300 shadow-lg hover:scale-105" />
+                <Button label='Search Property' className="px-8 py-4 mt-30 rounded-full text-white font-semibold bg-white/10 backdrop-blur-md border border-white/30 hover:bg-white/20 transition-all duration-300 shadow-lg hover:scale-105" />
             </div>
         </section>
     )
 }
 
-export default Hero
+export default Hero;
