d927d0b946a9a54c2488e214062a374b82f371b1
payaalarunsingh@gmail.com <payaalarunsingh>
2025-12-16 10:22:40 +0530

    Added AOS for scroll animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Header from './components/Header.jsx'
 import Hero from './components/Hero.jsx'
 import PopularProperties from './components/PopularProperties.jsx'
@@ -9,8 +9,25 @@ import Testimonials from './components/Testimonials.jsx'
 import Blog from './components/Blog.jsx'
 import Cta from './components/Cta.jsx'
 import Footer from './components/Footer.jsx'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+
+
 
 const App = () => {
+
+
+  useEffect(() => {
+    AOS.init(
+      {
+        duration: 800,
+        offset: 120,
+        easing: 'ease-out-cubic',
+        // once: true,
+      }
+    )
+  }, [])
+
   return (
     <>
     <Header/>
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -11,16 +11,21 @@ const Services = () => {
 
         {/* lists */}
         <ul className="grid gap-6 mt-10 md:mt-16 sm:grid-cols-2 xs:grid-cols-2 lg:grid-cols-4">
-          {ServicesCardItem.map(item => (
+          {ServicesCardItem.map((item, index) => (
             
             // card
-            <div className="border border-gray-300 p-9 rounded-lg bg-white hover:border-[#01296e] transition" key={item.id}>
+            <li 
+                className="border border-gray-300 p-9 rounded-lg bg-white hover:border-[#01296e] transition" 
+                key={item.id}
+                data-aos='fade-up'
+                data-aos-delay={index * 150} 
+                >
               <div className='bg-sky-200/30 max-w-max p-5 rounded-full font-bold my-3'>
                 <img src={item.icon} alt={item.title} width={70} height={70} />
               </div>
               <h4 className='text-[22px] text-[#01296e]'>{item.title}</h4>
               <p>{item.text}</p>
-            </div>
+            </li>
 
 
           ))}
